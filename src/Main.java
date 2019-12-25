757b80b00e85f127ca6f865d97b8abca12579f6d
tonnarough <58634989+tonnarough@users.noreply.github.com>
2019-12-25 16:02:52 +0300

    push in gthb

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
+
     public static void main(String[] args) {
         List<Food> breakfast = Arrays.stream(args).map(food -> {         
             String[] components = food.split("/");
